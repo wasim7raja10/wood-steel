30a88198f9381912bd87ad88444eb0ecce961419
Wasim Raja <wasim7raja10@gmail.com>
2023-01-23 19:36:16 +0530

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className=''>Hello</h1>
+        <h1 className=' text-red-500'>Hello</h1>
       </main>
     </>
   )
