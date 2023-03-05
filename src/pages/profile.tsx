c76f70399dccfa10cd72a07722eaf06930800cd7
Wasim Raja <wasim7raja10@gmail.com>
2023-03-05 13:04:57 +0530

    head in all pages

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Profile() {
   return (
     <>
+      <Head>
+        <title>Profile</title>
+      </Head>
       <h1>Profile</h1>
     </>
   );
--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head"
+
 export default function Wishlist() {
   return (
     <>
+      <Head>
+        <title>Wishlist</title>
+      </Head>
       <h1>Wishlist</h1>
     </>
   );
