60a9134a678c57ce1728232b3cda74dfd2f198d2
wasim7raja10 <wasim7raja10@gmail.com>
2023-03-29 14:30:20 +0530

    seed done

---
+++ prisma/data/category.mjs
@@ -0,0 +1,29 @@
+const categories = [
+  {
+    name: "Living Room",
+    slug: "living-room",
+    image: "https://example.com/images/living-room-furniture.jpg",
+  },
+  {
+    name: "Bedroom",
+    slug: "bedroom",
+    image: "https://example.com/images/bedroom-furniture.jpg",
+  },
+  {
+    name: "Dining Room",
+    slug: "dining-room",
+    image: "https://example.com/images/dining-room-furniture.jpg",
+  },
+  {
+    name: "Office",
+    slug: "office",
+    image: "https://example.com/images/office-furniture.jpg",
+  },
+  {
+    name: "Decor",
+    slug: "decor",
+    image: "https://example.com/images/decor.jpg",
+  }
+];
+
+export default categories;
