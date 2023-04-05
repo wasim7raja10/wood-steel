500462857e80f09314b27db0ca75b5e2adc2e5cc
wasim7raja10 <wasim7raja10@gmail.com>
2023-04-05 07:06:39 +0530

    get category by id

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,6 +1,29 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const categoryRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      const category = ctx.prisma.category.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+      const subcategories = ctx.prisma.subCategory.findMany({
+        where: {
+          categoryId: input.id,
+        },
+      });
+      return {
+        category,
+        subcategories,
+      };
+    }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.category.findMany();
   }),
